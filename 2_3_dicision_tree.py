361bc8f9e79255cd85d9c44c1aca5189b666c8bb
Tetsuya Katsuragi <webmaster.smashingpumpkins@gmail.com>
2019-12-22 21:14:26 +0900

    ファイル追加

--- 2_3_dicision_tree.py
+++ 2_3_dicision_tree.py
@@ -2,6 +2,9 @@
 # ## Dicision tree
 
 # %%
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.datasets import make_moons
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LinearRegression
 from sklearn.tree import DecisionTreeRegressor
 import os
@@ -104,5 +107,78 @@ plt.semilogy(test.date, test.price, label='Test data')
 plt.semilogy(ram_prices.date, price_tree, label='Tree prediction')
 plt.semilogy(ram_prices.date, price_lr, label='Linear prediction')
 plt.legend()
+
+# %%
+
+X, y = make_moons(n_samples=100, noise=0.25, random_state=3)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, stratify=y, random_state=42)
+forest = RandomForestClassifier(n_estimators=5, random_state=2)
+forest.fit(X_train, y_train)
+
+# %%
+fig, axes = plt.subplots(2, 3, figsize=(20, 10))
+for i, (ax, tree) in enumerate(zip(axes.ravel(), forest.estimators_)):
+    ax.set_title('Tree {}'.format(i))
+    mglearn.plots.plot_tree_partition(X_train, y_train, tree, ax=ax)
+
+mglearn.plots.plot_2d_separator(
+    forest, X_train, fill=True, ax=axes[-1, -1], alpha=.4)
+axes[-1, -1].set_title('Random Forest')
+mglearn.discrete_scatter(X_train[:, 0], X_train[:, 1], y_train)
+
+# %%
+X_train, X_test, y_train, y_test = train_test_split(
+    cancer.data, cancer.target, random_state=0)
+forest = RandomForestClassifier(n_estimators=100, random_state=0)
+forest.fit(X_train, y_train)
+
+print('Accuracy on training set: {:.3f}'.format(
+    forest.score(X_train, y_train)))
+print('Accuracy on test set: {:.3f}'.format(forest.score(X_test, y_test)))
+
+# %%
+plot_feature(forest)
+
+# %%
+X_train, X_test, y_train, y_test = train_test_split(
+    cancer.data, cancer.target, random_state=0)
+gbrt = GradientBoostingClassifier(random_state=0)
+gbrt.fit(X_train, y_train)
+
+print('Accuracy on training set: {:.3f}'.format(gbrt.score(X_train, y_train)))
+print('Accuracy on test set: {:.3f}'.format(gbrt.score(X_test, y_test)))
+
+# %%
+gbrt = GradientBoostingClassifier(random_state=0, max_depth=1)
+gbrt.fit(X_train, y_train)
+
+print('Accuracy on training set: {:.3f}'.format(gbrt.score(X_train, y_train)))
+print('Accuracy on test set: {:.3f}'.format(gbrt.score(X_test, y_test)))
+
+# %%
+gbrt = GradientBoostingClassifier(random_state=0, learning_rate=0.01)
+gbrt.fit(X_train, y_train)
+
+print('Accuracy on training set: {:.3f}'.format(gbrt.score(X_train, y_train)))
+print('Accuracy on test set: {:.3f}'.format(gbrt.score(X_test, y_test)))
+
+
+# %%
+gbrt = GradientBoostingClassifier(random_state=0, max_depth=1)
+gbrt.fit(X_train, y_train)
+
+plot_feature(gbrt)
+
+# %%
+
+X_train, X_test, y_train, y_test = train_test_split(
+    cancer.data, cancer.target, random_state=0)
+
+gbrt = GradientBoostingClassifier(random_state=0)
+gbrt.fit(X_train, y_train)
+
+print('Accuracy on training set: {:.3f}'.format(gbrt.score(X_train, y_train)))
+print('Accuracy on test set: {:.3f}'.format(gbrt.score(X_test, y_test)))
 
 # %%
